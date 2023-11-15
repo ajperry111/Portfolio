3778120449cb8d28688f78fc14663c9910917687
ajperry111 <113034599+ajperry111@users.noreply.github.com>
2023-11-15 08:37:34 -0600

    Relocate

---
+++ Python/PasswordManager/Account.py
@@ -0,0 +1,8 @@
+class Account:
+  @staticmethod
+  def printAccount(name,username,password,category):
+    print(f"Site Name: {name}\n Username: {username}\n Password: {password}\n Category: {category}")
+    
+
+  
+      
