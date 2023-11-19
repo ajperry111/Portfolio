2f04ade6e4924c57cdbe59b0f2d5613a904981dd
ajperry111 <113034599+ajperry111@users.noreply.github.com>
2023-11-18 20:51:35 -0500

    Update InvetoryArrayList.java

--- Java/InvetoryArrayList.java
+++ Java/InvetoryArrayList.java
@@ -2,46 +2,65 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class InvetoryArrayList {
-     public static void main(String[] args) {
-          //Program that keeps track of the animals at the Vet
-          //   add, insert, remove, replace, clear the db
-          //   if the user types in quit -> the program ends  
-          /*Finish the starter code attached Invetory.java utilizing two different versions.  
-          The first version should be built utilizing the ArrayList petList (ArrayList<String> petList).  
-          The second version is utilizing Arrays petList(String[] petList).  
-          Please indicate the which file is which by naming it InvetoryArray.java and InvetoryArrayList.java.  
+    public static void main(String[] args) {
+        ArrayList<String> petList = new ArrayList<String>();
+        Scanner ui = new Scanner(System.in);
+        String userInput = "";
 
-          What to submit:
-          Both versions of the program.
-          A slideshow comparing each action (add, insert, remove, replace, clear, view) of the programs.  
-          For example, on one slide you can show the algorithm to "add" an item to the list. 
-          On that same slide you will need to explain 2 difference and 2 similarities between the two data types.  Please explain in detail. */    
-          ArrayList<String> petList = new ArrayList<String>();
-          Scanner ui = new Scanner(System.in);
-          String userInput = "";
+        while (!userInput.equals("q")) {
+            System.out.println("What would you like to do? (a)dd, (i)nsert, (r)emove, Re(p)lace, (c)lear, (v)iew, or (q)uit");
+            userInput = ui.nextLine();
+          
+            if (userInput.equals("a")) {
+                System.out.print("Enter your pet's name that you want to add: ");
+                String name = ui.nextLine();
+                petList.add(name);
+                System.out.println(name + " added to the database!");
+            } else if (userInput.equals("i")) {
+                System.out.print("Enter the position where you want to insert: ");
+                int position = Integer.parseInt(ui.nextLine())-1;
 
-          while(!userInput.equals("q")){
-               System.out.println("What would you like to do? (a)dd,(i)nsert,(r)emove,Re(p)lace,(c)lear,(v)iew, or (q)uit");
-               userInput=ui.nextLine();
+                if (position >= 0 && position <= petList.size()) {
+                    System.out.print("Enter the pet's name to insert: ");
+                    String name = ui.nextLine();
+                    petList.add(position, name);
+                    position++;
+                    System.out.println(name + " inserted at " + position);
+                } else {
+                    System.out.println("Invalid position.");
+                }
+            } else if (userInput.equals("r")) {
+                System.out.print("Enter the pet's name to remove: ");
+                String name = ui.nextLine();
 
-               if(userInput.equals("a")){
-                    System.out.println("add");
-               }else if(userInput.equals("i")){
-                    System.out.println("insert");
-               }else if(userInput.equals("r")){
-                    System.out.println("remove");
-               }else if(userInput.equals("p")){
-                    System.out.println("replace");
-               }else if(userInput.equals("c")){
-                    System.out.println("clear");
-               }else if(userInput.equals("v")){
-                    System.out.println("view");
-               }else{
-                    System.out.println("Please choose a correct answer");
-               }
-               
-               userInput = ui.nextLine();
-          }
-          System.out.println("bye bye");
-     }     
+                if (petList.contains(name)) {
+                    petList.remove(name);
+                    System.out.println(name + " removed from the database.");
+                } else {
+                    System.out.println("Animal not found.");
+                }
+            } else if (userInput.equals("p")) {
+                System.out.print("Who would you like to replace? ");
+                String replacee = ui.nextLine();
+
+                if (petList.contains(replacee)) {
+                    System.out.print("What name would you like to replace it with? ");
+                    String replacer = ui.nextLine();
+                    int index = petList.indexOf(replacee);
+                    petList.set(index, replacer);
+                    System.out.println(replacee + " replaced with " + replacer);
+                } else {
+                    System.out.println("Animal not found.");
+                }
+            } else if (userInput.equals("c")) {
+                petList.clear();
+                System.out.println("Database cleared.");
+            } else if (userInput.equals("v")) {
+                System.out.println("Current database: " + petList);
+            } else if (!userInput.equals("q")){
+                System.out.println("Please choose a correct option.");
+            }
+        }
+        System.out.println("bye bye");
+    }
 }
